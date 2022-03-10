f08631af2ed19e7480ce34199239ace5dfa1cc90
khalidh <khalidh@directferries.com>
2022-03-10 16:40:57 +0000

    remove print statements

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def main():
     aws_db_table_users = db.Table("users")
     aws_users = aws_db_table_users.scan()["Items"]
     for user in aws_users:
-        print(user)
+        # print(user)
         try:
             relevant_jobs_found = get_relevant_jobs(db, user)
-            print(relevant_jobs_found)
+            # print(relevant_jobs_found)
             send_daily_job_alerts(db, user, html_email)
             save_sent_alerts(db, user, relevant_jobs_found)
         except Exception as e:
